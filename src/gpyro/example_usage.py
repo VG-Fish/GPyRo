930c4896db45ff5789a737f8abcd1c5bb7978165
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-05-24 11:07:22 -0500

    fixed bug in roblox scraper

--- src/gpyro/example_usage.py
+++ src/gpyro/example_usage.py
@@ -1,12 +1,13 @@
 from roblox_scraper import RobloxGameDataScraper
 from rolimons_scraper import RolimonsScraper, RolimonsGamePlaceIdsType
+import asyncio
 
 def main() -> None:
     rolimons_scraper: RolimonsScraper = RolimonsScraper()
     game_place_ids: RolimonsGamePlaceIdsType = rolimons_scraper.get_game_place_ids()
 
     roblox_scraper: RobloxGameDataScraper = RobloxGameDataScraper()
-    roblox_scraper.get_games(game_place_ids, ignore_conversion_errors=True)
+    asyncio.run(roblox_scraper.get_games(game_place_ids, ignore_conversion_errors=True))
     roblox_scraper.save_game_data("rolimons_6180_games_data.parquet")
 
 if __name__ == "__main__":
--- src/gpyro/roblox_scraper.py
+++ src/gpyro/roblox_scraper.py
@@ -1,10 +1,13 @@
 from typing import Self, List, Dict
 from errors import UnableToReachURL
 from rolimons_scraper import RolimonsGamePlaceIdsType
-import requests as r
-import pandas as pd
 from time import sleep
 from random import random
+import asyncio
+
+import requests as r
+import pandas as pd
+from aiohttp import ClientSession #pyright: ignore
 
 class RobloxGameDataScraper:
     def __init__(self: Self) -> None:
@@ -12,8 +15,30 @@ class RobloxGameDataScraper:
         self._game_data_url: str = "https://games.roblox.com/v1/games?universeIds={universe_ids}"
         self._game_votes_data_url: str = "https://games.roblox.com/v1/games/votes?universeIds={universe_ids}"
         self._cache: Dict = dict()
+        self._universe_ids: List[str] = []
 
-    def get_games(
+    async def _fetch_universe_id(self: Self, place_id: int, session: ClientSession, ignore_conversion_errors: bool = False) -> int | None:
+        sleeping_time: float = random() * 0.1
+        print(f"Sleeping for {sleeping_time}s.")
+        sleep(sleeping_time)
+
+        place_id_to_universe_id_url: str = self._place_id_to_universe_id_url.format(place_id=place_id)
+        print(f"Currently grabbing Roblox {place_id_to_universe_id_url}")
+
+        async with session.get(place_id_to_universe_id_url) as response:
+            json = await response.json()
+
+            if "universeId" not in json:
+                if ignore_conversion_errors:
+                    print(f"Error reaching {place_id_to_universe_id_url}!")
+                    return
+                else:
+                    raise UnableToReachURL("Error: Unable to get universeId information.")
+
+        universe_id: int = json["universeId"]
+        self._universe_ids.append(str(universe_id))
+
+    async def get_games(
         self: Self,
         game_place_ids: RolimonsGamePlaceIdsType,
         amount: int | None = None,
@@ -27,40 +52,28 @@ class RobloxGameDataScraper:
         ):
             raise ValueError(f"Parameter 'amount' must be a positive whole number that is less than {len(game_place_ids)}.")
 
-        universe_id_urls: List[str] = []
         print("Converting Roblox place_ids to universe_ids...")
-        for place_id, _ in zip(game_place_ids, range(amount)):
-            sleeping_time: float = 0.1 + random()
-            print(f"Sleeping for {sleeping_time}s.")
-            sleep(sleeping_time)
 
-            place_id_to_universe_id_url: str = self._place_id_to_universe_id_url.format(place_id=place_id)
-            print(f"Currently grabbing Roblox {place_id_to_universe_id_url}...")
-            json = r.get(place_id_to_universe_id_url).json()
+        async with ClientSession() as session:
+            tasks: List = [
+                self._fetch_universe_id(place_id, session, ignore_conversion_errors)
+                for place_id, _ in zip(game_place_ids, range(amount))
+            ]
+            await asyncio.gather(*tasks)
 
-            if "universeId" not in json:
-                if ignore_conversion_errors:
-                    print(f"Error reaching {place_id_to_universe_id_url}!")
-                    continue
-                else:
-                    raise UnableToReachURL("Error: Unable to get universeId information.")
-
-            universe_id: int = json["universeId"]
-            universe_id_urls.append(str(universe_id))
-
-        universe_ids: str = ",".join(universe_id_urls)
+        universe_ids: str = ",".join(self._universe_ids)
         universe_id_url: str = self._game_data_url.format(universe_ids=universe_ids)
         universe_votes_id_url: str = self._game_votes_data_url.format(universe_ids=universe_ids)
 
         print("Currently grabbing Roblox game data from universe ids...")
-        games = r.get(universe_id_url).json()
-        votes = r.get(universe_votes_id_url).json()
+        games: Dict = r.get(universe_id_url).json()
+        votes: Dict = r.get(universe_votes_id_url).json()
 
         if  "data" not in games or "data" not in votes:
             raise UnableToReachURL("Error: Unable to get game information.")
 
         results: List[Dict] = []
-        for game, vote in zip(games, votes):
+        for game, vote in zip(games["data"], votes["data"]):
             results.append(game | vote)
         self._cache["game_data"] = results
 
